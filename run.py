6a4e7706c5455b0ddb11576c80e3e175169d48cd
Jake Summerville <jsvikings97@gmail.com>
2022-05-09 16:59:25 -0700

    Add disk selection

--- run.py
+++ run.py
@@ -20,10 +20,64 @@ def get_arguments():
     return parser.parse_args()
 
 
+def select_disk():
+    """ Select disk to use for bootable media """
+    result = subprocess.run(['df', '-h'], capture_output=True, text=True)
+
+    if result.returncode != 0:
+        print('Unable to detect media')
+        sys.exit()
+
+    disk_list = result.stdout.split('\n')
+    parsed_disk_list = []
+
+    # parse disks that can't be used out
+        disk = disk.strip()
+        if disk == disk_list[0] or '/dev/disk' in disk:
+            parsed_disk_list.append(disk)
+
+    # display possible disk options
+    print('\nDisk selection for bootable media\n')
+    print('  ', parsed_disk_list[0])
+    for i in range(1, len(parsed_disk_list)):
+        print(str(i) + ')', parsed_disk_list[i])
+
+    # prompt use for disk selection and confirmation
+    while True:
+        selection = input('\nSelect which disk to use: ')
+        try: 
+            selection = int(selection)
+        except:
+            print('Please enter a number')
+            continue
+
+        max_select = len(parsed_disk_list) - 1
+        if selection < 1 or selection > max_select:
+            print('Please enter a number between 1 and', max_select)
+            continue
+
+        selected_disk = parsed_disk_list[selection].split()[0] 
+        print('You selected', str(selection) + ':', selected_disk)
+        
+        confirm = input('\nIs this correct (y/n): ')
+
+        if confirm.lower() == 'y':
+            break
+
+    print('')
+
+    selected_disk = selected_disk.replace('/dev/disk', '')
+
+    if 's' in selected_disk:
+        selected_disk = selected_disk.split('s')[0]
+
+    return selected_disk
+
+
 def cli_command(opts):
     """ Run command line interface commands """
-    output = subprocess.run(opts, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    return output.returncode
+    result = subprocess.run(opts, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    return result.returncode
 
 
 def convert_ISO (iso_file, img_file, dmg_file):
@@ -71,27 +125,30 @@ def create_installer (file, disk):
 
 def main ():
     """ Main function for the media creation tool """ 
+
+    print('\n=====================')
+    print(' Media Creation Tool')
+    print('=====================\n')
+
     args = get_arguments()
 
     if not (args.file and os.path.isfile(args.file)):
         print('Please enter an iso file')
         sys.exit()
 
-    if not args.disk:
-        print('Please enter the disk number of the flash drive')
-        sys.exit()
-
-    print('\n=====================')
-    print(' Media Creation Tool')
-    print('=====================\n')
+    if args.disk:
+        disk = str(args.disk)
+    else:
+        disk = select_disk()
 
     iso_file = str(args.file)
     img_file = iso_file.replace('.iso', '.img')
     dmg_file = img_file + '.dmg'
-    disk = str(args.disk)
 
     print('ISO:', iso_file)
     print('Disk:', disk, '\n')
+
+    return
 
     if os.path.isfile(img_file):
         os.remove(img_file)
